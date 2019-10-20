5d35cbe1ad555576084f3d9d69583a62f1b87475
Andrew Boyce <sigusrbacon@github.com>
2019-10-20 21:40:30 +0100

    its pretty basic...

---
+++ circulate.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import sys
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+
+RELAIS_1_GPIO = 5
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+
+if len(sys.argv) == 1:
+    print("Must supply argument 'on' or 'off")
+    sys.exit(1)
+
+
+if sys.argv[1] == 'off':
+    GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+elif sys.argv[1] == 'on':
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
